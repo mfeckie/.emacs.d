40cfc75203ff13bb1a943867b193559ad4a43323
Martin Feckie <mfeckie@gmail.com>
2015-09-20 10:15:23 +1000

    Update my config

---
+++ personal/martins-config.el
@@ -0,0 +1,10 @@
+;;; package --- Martin's config
+
+(global-linum-mode 1)
+
+(provide 'martins-config)
+
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
+;;; martins-config ends here
